057bc863c2f2045f4f850473f00f27b78f4dad52
Pearl McPhee <pearlmcphee@gmail.com>
2015-11-11 11:50:00 -0800

    Pearl: fixed linkedin bug. users now their names and profiles available

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
-var bcrypt = require('bcrypt-nodejs');
 var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
@@ -62,29 +61,7 @@ UserSchema.methods.generateJWT = function() {
  },  "CoderCamps"); //Add Passcode here
 };
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
 
-// UserSchema.pre('save', function(next) {
-//   var user = this;
-//   var SALT_FACTOR = 5;
-//
-//   if (!user.isModified('password')) return next();
-//
-//   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-//     if (err) return next(err);
-//
-//     bcrypt.hash(user.password, salt, null, function(err, hash) {
-//       if (err) return next(err);
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
 
 
 mongoose.model('User', UserSchema);
