3728f4adc21f3c2c5dc782c9d90b891e746cc144
pearl1991 <pearlmcphee@gmail.com>
2015-11-20 17:55:06 -0800

    fixed mergin issue in homecontroller

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -1,55 +1,4 @@
 (function() {
-<<<<<<< HEAD
-	'use strict';
-	angular.module('app')
-	.controller('HomeController', HomeController);
-
-	function HomeController(HomeFactory, UserFactory,  $state) {
-		var vm = this;
-		    vm.status = UserFactory.status;
-				vm.post = {};
-				vm.allPosts = {};
-				vm.allPosts.reblog = false;
-				vm.repost = {};
-			
-
-vm.getPost = function(){
-		HomeFactory.getAllPosts().then(function(res){
-  			vm.allPosts=res;
-		});
-}
-vm.getPost();
-
-		vm.createPost = function (){
-		HomeFactory.postPost(vm.post).then(function(res){
-			vm.allPosts.push(vm.post);
-			vm.post = {};
-			vm.getPost(); //this fixes our posted on invalide date bug
-
-		});
-		};
-
-vm.reblog = function(post){
-	HomeFactory.reblog(post._id, vm.repost)
-	.then(function(res){
-		console.log("Will figure this out later");
-	})
-}
-    vm.contact = function() { // this is for the mock contact form we have.
-      $state.go("Home");
-    };
-
-		vm.followOnPost = function(post){
-			UserFactory.followOnPost(post.creatorId, vm.status)
-			.then(function(res){
-				console.log("You have a new follower");
-			});
-		};
-
-
-	}
-})();
-=======
   'use strict';
   angular.module('app')
     .controller('HomeController', HomeController);
@@ -137,4 +86,3 @@ vm.reblog = function(post){
 
   }
 })();
->>>>>>> 0cb1317cdec04863603ec5fddc2f99949ec27b63
