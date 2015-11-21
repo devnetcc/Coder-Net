93cc4ca47965bce89ee07000fd20081d6cc64918
Ghazal Aghvami <ghazal.aghvami@gmail.com>
2015-11-21 15:41:12 -0800

    profile bugs fixed

--- public/javascript/services/HomeFactory.js
+++ public/javascript/services/HomeFactory.js
@@ -6,9 +6,9 @@
   function HomeFactory($http, $q) {
     var o = {};
 
-    o.postPost = function(post, creator) {
+    o.postPost = function(post) {
       var q = $q.defer();
-      $http.post('api/posts', post, creator).then(function(res) {
+      $http.post('api/posts', post).then(function(res) {
         q.resolve(res.data);
       });
       return q.promise;
@@ -39,6 +39,14 @@
     o.getAllPosts = function() {
       var q = $q.defer();
       $http.get('/api/posts').then(function(res) {
+        q.resolve(res.data);
+      });
+      return q.promise;
+    };
+
+    o.getProfilePosts = function(id){
+      var q = $q.defer();
+      $http.get('/api/posts/userPosts/'+ id).then(function(res){
         q.resolve(res.data);
       });
       return q.promise;
